233cfd135955fe5357d1260d2760bb137e1d374d
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-05-26 10:03:25 -0500

    added modulus math to get cards printing three to a row

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -2,7 +2,10 @@ const movieOutputDiv = $('#movies');
 
 const domString = (movieArray, config) => {
   let movieStrang = '';
-  movieArray.forEach((movie) => {
+  movieArray.forEach((movie, index) => {
+    if (index % 3 === 0) {
+      movieStrang += `<div class="row">`;
+    }
     movieStrang += `<div class="col-sm-6 col-md-4">`;
     movieStrang += `<div class="thumbnail">`;
     movieStrang += ` <img src="${config.base_url}/w342/${movie.poster_path}" alt="Movie Poster">`;
@@ -13,6 +16,10 @@ const domString = (movieArray, config) => {
     movieStrang += `</div>`;
     movieStrang +=  `</div>`;
     movieStrang +=  `</div>`;
+
+    if (index % 3 === 2) {
+      movieStrang += `</div>`;
+    }
   });
   printToDom(movieStrang);
 };
