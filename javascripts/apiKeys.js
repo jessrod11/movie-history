9d038ef83fadf17a48a80bfda38087f4b237e16e
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-05-31 20:04:58 -0500

    added saving movies work from Tues

--- javascripts/apiKeys.js
+++ javascripts/apiKeys.js
@@ -1,4 +1,5 @@
 const tmdb = require('./tmdb');
+const firebaseApi = require('./firebaseApi');
 
 const apiKeys = () => {
   return new Promise ((resolve, reject) => {
@@ -17,6 +18,7 @@ const retrieveKeys = () => {
     .then((results) => {
       tmdb.setKey(results.tmdb.apiKey);
       firebase.initializeApp(results.firebase);
+      firebaseApi.setConfig(results.firebase);
     })
     .catch((err) => {
       console.error('no keys', err);
